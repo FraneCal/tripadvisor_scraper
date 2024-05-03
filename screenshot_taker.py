9192d25d3b21b5cfcf410efedc94e496e73f2c2b
Frane Ćalušić <90317417+FraneCal@users.noreply.github.com>
2024-05-03 13:44:11 +0200

    Update screenshot_taker.py

--- screenshot_taker.py
+++ screenshot_taker.py
@@ -27,3 +27,37 @@ for url in urls:
     driver.save_screenshot(screenshot_path)
 
     soup = BeautifulSoup(page_source, "html.parser")
+
+
+# --------------- SELENIUM --------------- #
+# from selenium import webdriver
+# import time
+
+# URL = 'https://www.tripadvisor.com//Restaurant_Review-g186338-d1770199-Reviews-Ecco_Pizza-London_England.html'
+# driver = webdriver.Chrome()
+# driver.get(URL)
+# time.sleep(5)
+# driver.save_screenshot('image.png')
+# driver.quit()
+
+
+
+    
+
+# --------------- PLAYWRIGHT --------------- #
+# from playwright.sync_api import sync_playwright
+
+# URL = 'https://www.tripadvisor.com//Restaurant_Review-g186338-d1770199-Reviews-Ecco_Pizza-London_England.html'
+
+# with sync_playwright() as p:
+#     browser = p.chromium.launch(headless=False)
+#     # Create a new page
+#     page = browser.new_page()
+#     # Navigate to the URL
+#     page.goto(URL)
+#     # Wait for some time (if needed)
+#     page.wait_for_timeout(5000)
+#     # Take a screenshot
+#     page.screenshot(path='image.png')
+#     # Close the browser
+#     browser.close()
