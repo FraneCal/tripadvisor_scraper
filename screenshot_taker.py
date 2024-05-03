ea50c028c57cfdd66796df13f0fb8877e185ead3
Frane Ćalušić <90317417+FraneCal@users.noreply.github.com>
2024-05-03 15:23:13 +0200

    Update screenshot_taker.py

--- screenshot_taker.py
+++ screenshot_taker.py
@@ -1,63 +1,31 @@
-from bs4 import BeautifulSoup
-from selenium import webdriver
-import os
+import csv
+from playwright.sync_api import sync_playwright
 
-def get_restaurant_name(url):
-    start_index = url.find('Reviews-') + len('Reviews-')
-    end_index = url.find('-London')
-    return url[start_index:end_index]
+# Read URLs from the CSV file
+urls = []
+with open("links_for_screenshots.csv", "r") as file:
+    reader = csv.reader(file)
+    for row in reader:
+        urls.append(row[0])
 
-with open("restaurant_links.txt", "r") as file:
-    urls = file.readlines()
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
 
-# Instantiate the web driver outside the loop
-driver = webdriver.Chrome()
+    for url in urls:
+        # Create a new page
+        page = browser.new_page()
 
-for url in urls:
-    url = url.strip()
+        # Navigate to the URL
+        page.goto(url)
 
-    driver.get(url)
+        # Wait for some time (if needed)
+        page.wait_for_timeout(5000)
 
-    page_source = driver.page_source
+        # Take a screenshot
+        page.screenshot(path=f'{url.replace("/", "_").replace(":", "_")}.png')
 
-    restaurant_name = get_restaurant_name(url)
-    screenshot_filename = f"{restaurant_name}.png"
+        # Close the page
+        page.close()
 
-    screenshot_path = os.path.join("screenshots", screenshot_filename)
-    driver.save_screenshot(screenshot_path)
-
-    soup = BeautifulSoup(page_source, "html.parser")
-
-
-# --------------- SELENIUM --------------- #
-# from selenium import webdriver
-# import time
-
-# URL = 'https://www.tripadvisor.com//Restaurant_Review-g186338-d1770199-Reviews-Ecco_Pizza-London_England.html'
-# driver = webdriver.Chrome()
-# driver.get(URL)
-# time.sleep(5)
-# driver.save_screenshot('image.png')
-# driver.quit()
-
-
-
-    
-
-# --------------- PLAYWRIGHT --------------- #
-# from playwright.sync_api import sync_playwright
-
-# URL = 'https://www.tripadvisor.com//Restaurant_Review-g186338-d1770199-Reviews-Ecco_Pizza-London_England.html'
-
-# with sync_playwright() as p:
-#     browser = p.chromium.launch(headless=False)
-#     # Create a new page
-#     page = browser.new_page()
-#     # Navigate to the URL
-#     page.goto(URL)
-#     # Wait for some time (if needed)
-#     page.wait_for_timeout(5000)
-#     # Take a screenshot
-#     page.screenshot(path='image.png')
-#     # Close the browser
-#     browser.close()
+    # Close the browser
+    browser.close()
